23c2a393b5667b9d246d378084898ef758276824
Bertrand <bneron@pasteur.fr>
2020-02-03 13:15:26 +0100

    :pencil2: add licence information in grep_vcf file

--- grep_vcf/scripts/grep_vcf.py
+++ grep_vcf/scripts/grep_vcf.py
@@ -1,16 +1,40 @@
+#########################################################################
+# grep_vcf - remove line fom vcf file where positions are not in        #
+# reference file                                                        #
+# Authors: Bertrand Neron                                               #
+# Copyright (c) 2020  Institut Pasteur (Paris) and CNRS.                #
+# See the COPYRIGHT file for details                                    #
+#                                                                       #
+# This file is part of grep_vcf package.                                #
+#                                                                       #
+# grep_vcf is free software: you can redistribute it and/or modify      #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# grep_vcf is distributed in the hope that it will be useful,           #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+# GNU General Public License for more details .                         #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with MacSyFinder (COPYING).                                     #
+# If not, see <https://www.gnu.org/licenses/>.                          #
+#########################################################################
+
 import sys
 import os
 import argparse
-
+import grep_vcf
 import grep_vcf.grep_vcf as gv
 
 
 def get_version_message():
     """
-
-    :return:
+    :return: the version informations
+    :rtype: str
     """
-    version = gv.__version__
+    version = grep_vcf.__version__
     vers_msg = f"""grep_vcf {version}
 Python {sys.version}
 
