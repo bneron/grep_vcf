f08c0cc5a83c8186cfbac064cb9840376c058216
Bertrand <bneron@pasteur.fr>
2020-02-03 13:12:39 +0100

    :art: mv version at package level

---
+++ grep_vcf/__init__.py
@@ -0,0 +1,25 @@
+#########################################################################
+# grep_vcf - remove line fom vcf file where positions are not in        #
+# reference file                                                        #
+# Authors: Bertrand Neron                                               #
+# Copyright (c) 2020  Institut Pasteur (Paris) and CNRS.                #
+# See the COPYRIGHT file for details                                    #
+#                                                                       #
+# This file is part of grep_vcf package.                                #
+#                                                                       #
+# grep_vcf is free software: you can redistribute it and/or modify      #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# grep_vcf is distributed in the hope that it will be useful,           #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+# GNU General Public License for more details .                         #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with MacSyFinder (COPYING).                                     #
+# If not, see <https://www.gnu.org/licenses/>.                          #
+#########################################################################
+
+__version__ = 0.1
